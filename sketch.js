06507af4c1f9dd6580b648a87a89e580620e530e
DHAIRYA-KAPRE <73997306+DHAIRYA-KAPRE@users.noreply.github.com>
2020-11-29 11:50:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+const Engine = Matter.Engine; 
+ const World = Matter.World; 
+ const Bodies = Matter.Bodies; 
+ var engine,world,body; 
+ function setup(){ createCanvas(400,400);
+   engine = Engine.create();
+    world = engine.world;
+    body = Bodies.rectangle(200,200,100,100);
+     World.add(world,body);
+     }
+      function draw(){ background("orange");
+     Engine.update(engine)
+      rectMode(CENTER)
+      rect(200,body.position.y,100,100); 
+    
+    }
